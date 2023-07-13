475085d837a2c16ceb34060cbaaf7a42bd41665f
Dalvis <Dalvis.liz27@gmail.com>
2023-07-13 14:58:57 -0400

    added routers and first migration file

--- api/server.js
+++ api/server.js
@@ -1,1 +1,20 @@
 // build your server here and require it from index.js
+const express = require('express');
+const ResourcesRouter = require('./resource/router');
+const TasksRouter = require('./task/router');
+const ProjectsRouter = require('./project/router');
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/resources', ResourcesRouter);
+server.use('/api/tasks', TasksRouter);
+server.use('/api/projects', ProjectsRouter);
+
+server.use('*', (req, res) => {
+    res.json({ 
+        message: 'API is up and running!' 
+    });
+});
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // start your server here
+require('dotenv').config();
+
+const server = require('./api/server.js');
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+    console.log(`\n*** Server Running on [\x1b[32mhttp://localhost:\x1b[0m\x1b[34m${port}\x1b[0m\x1b[37m] ***\x1b[0m\n`);
+});
