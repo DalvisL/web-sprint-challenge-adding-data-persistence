e302dd601c65d37b6b51c2e1f9959391becba9fa
Dalvis <Dalvis.liz27@gmail.com>
2023-07-13 17:23:31 -0400

    Added the project model and router endpoints

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,36 @@
 // build your `Project` model here
+
+const db = require('../../data/dbConfig');
+
+async function getProjects() {
+    const project =  await db('projects');
+
+    const projectWithBoolean = project.map(project => {
+        return {
+            ...project,
+            project_completed: project.project_completed ? true : false
+        };
+    });
+
+    return projectWithBoolean;
+}
+
+async function addProject(project) {
+    await db('projects').insert(project);
+
+    const newProject = await db('projects')
+        .where('project_name', project.project_name)
+        .first();
+
+    const newProjectWithBoolean = {
+        ...newProject,
+        project_completed: newProject.project_completed ? true : false
+    };
+
+    return newProjectWithBoolean;
+}
+
+module.exports = {
+    getProjects,
+    addProject
+};
--- api/project/router.js
+++ api/project/router.js
@@ -1,7 +1,33 @@
 // build your `/api/projects` router here
 const express = require('express');
-
+const Project = require('./model');
 const router = express.Router();
+
+router.get('/', (req, res) => {
+    Project.getProjects()
+        .then(projects => {
+            res.status(200).json(projects);
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Error retrieving project',
+                err: err.message
+            });
+        });
+});
+
+router.post('/', (req, res) => {
+    Project.addProject(req.body)
+        .then(project => {
+            res.status(201).json(project);
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Error adding project',
+                err: err.message
+            });
+        });
+});
 
 
 
