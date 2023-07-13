4c29c7d2c2c44df94172de2615d5a9b2d08c698b
Dalvis <Dalvis.liz27@gmail.com>
2023-07-13 17:35:43 -0400

    added resource model, and router endpoints

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,23 @@
 // build your `Resource` model here
+const db = require('../../data/dbConfig');
+
+async function getResources() {
+    const resource = await db('resources');
+
+    return resource;
+}
+
+async function addResource(resource) {
+    await db('resources').insert(resource);
+
+    const newResource = await db('resources')
+        .where('resource_name', resource.resource_name)
+        .first();
+
+    return newResource;
+}
+
+module.exports = {
+    getResources,
+    addResource
+};
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,7 +1,33 @@
 // build your `/api/resources` router here
 const express = require('express');
-
+const Resource = require('./model');
 const router = express.Router();
+
+router.get('/', (req, res) => {
+    Resource.getResources()
+        .then(resources => {
+            res.status(200).json(resources);
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Error retrieving resources',
+                err: err.message
+            });
+        });
+});
+
+router.post('/', (req, res) => {
+    Resource.addResource(req.body)
+        .then(resource => {
+            res.status(201).json(resource);
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Error adding resource',
+                err: err.message
+            });
+        });
+});
 
 
 
