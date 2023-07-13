0034985071769e7959086fa8738e69db40b9820e
Dalvis <Dalvis.liz27@gmail.com>
2023-07-13 17:40:09 -0400

    added tasks router endpoints and model

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,44 @@
 // build your `Task` model here
+const db = require('../../data/dbConfig');
+
+async function getTasks() {
+    const task = await db('tasks as t')
+        .join('projects as p', 'p.project_id', 't.project_id')
+        .select(
+            't.task_id',
+            't.task_description',
+            't.task_notes',
+            't.task_completed',
+            'p.project_name',
+            'p.project_description'
+        );
+
+    const taskWithBoolean = task.map(task => {
+        return {
+            ...task,
+            task_completed: task.task_completed ? true : false
+        };
+    });
+
+    return taskWithBoolean;
+}
+
+async function addTask(task) {
+    await db('tasks').insert(task);
+
+    const newTask = await db('tasks')
+        .where('task_description', task.task_description)
+        .first();
+
+    const newTaskWithBoolean = {
+        ...newTask,
+        task_completed: newTask.task_completed ? true : false
+    };
+
+    return newTaskWithBoolean;
+}
+
+module.exports = {
+    getTasks,
+    addTask
+};
--- api/task/router.js
+++ api/task/router.js
@@ -1,8 +1,33 @@
 // build your `/api/tasks` router here
 const express = require('express');
-
+const Task = require('./model');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    Task.getTasks()
+        .then(tasks => {
+            res.status(200).json(tasks);
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Error retrieving tasks',
+                err: err.message
+            });
+        });
+});
+
+router.post('/', (req, res) => {
+    Task.addTask(req.body)
+        .then(task => {
+            res.status(201).json(task);
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Error adding task',
+                err: err.message
+            });
+        });
+});
 
 
 module.exports = router;
